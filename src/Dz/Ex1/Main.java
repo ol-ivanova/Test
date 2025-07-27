aa791089b4fe4d9bb8ab0cd746ccd2877305bbd1
oly <olgaivanova.19@icloud.com>
2025-07-27 11:59:05 +0300

    feat4

--- src/Dz/Ex1/Main.java
+++ src/Dz/Ex1/Main.java
@@ -4,16 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main implements Pair <String, Integer> {
-    Map<Integer, String> map = new HashMap<>();
+    Map<String, Integer> map = new HashMap<>();
 
     @Override
     public String getKey() {
-        return "";
+        return map.keySet();
     }
 
     @Override
     public Integer getValue() {
         return 0;
+    }
+
+    public Main(Map<String, Integer> map) {
+        this.map = map;
     }
 
     public static void main(String[] args) {
@@ -22,6 +26,10 @@ public class Main implements Pair <String, Integer> {
         map1.put("Вторник", 2);
         map1.put("Среда", 3);
         map1.put("Четверг", 4);
+
+        Main main1 = new Main(map1);
+
+
 
 
     }
